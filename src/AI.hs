07c5dcf22110f2172aa27f0fa4260f3b3bffd96e
Zach Cleghern <zbcleghern42@students.tntech.edu>
2015-05-06 23:24:12 -0500

    changes to AI

--- src/AI.hs
+++ src/AI.hs
@@ -8,64 +8,46 @@ import Control.Applicative
 --minimax :: Game -> Move
 --minimax g = maxGame $ map (\mv -> (mmMinNode 3 (move g mv), mv)) (nextMoves g)
   --      where maxGame = snd . foldr foldingF (-40, Move (Coord 1 1) (Coord 1 1))
-  --            foldingF x y = if fst x < fst y then y else x
-
-noGame :: Game
-noGame = Game blankBoard [] White
+    --          foldingF x y = if fst x < fst y then y else x
 
 --mmMaxNode :: Int -> Maybe Game -> Int
---mmMaxNode 0 (Just (Game b fl trn)) = evalNaive' b trn
+--mmMaxNode 0 (Just (Game b fl trn)) = evalNaive b trn
 --mmMaxNode d g = maximum (map (mmMinNode (d - 1)) (nextMoves g))
 
 --mmMinNode :: Int -> Maybe Game -> Int
---mmMinNode 0 (Just (Game b fl trn)) = evalNaive' b (notC trn)
+--mmMinNode 0 (Just (Game b fl trn)) = evalNaive b (notC trn)
 --mmMinNode d g = minimum (map (mmMaxNode (d - 1)) (nextMoves g))
 
-
-values :: Game -> [Int]
-values = map (\(Game brd _ trn) -> evalNaive' brd trn) . children
-
-printout :: Game -> [(Int, Move)]
-printout g = map (\mv -> (abMinNode 5 (-100) 100 (move g mv), mv)) (nextMoves g)
-        where maxGame = snd . foldr foldingF (-100, Move (Coord 1 1) (Coord 1 1))
-              foldingF x y = if fst y > fst x then y else x
-
 alphaBeta :: Game -> Move
-alphaBeta g = maxGame $ map (\mv -> (abMinNode 6 (-100) 100 (move g mv), mv)) (nextMoves g)
+alphaBeta g = maxGame $ map (\mv -> (abNode 6 (-100) 100 (move g mv) False, mv)) (nextMoves g)
         where maxGame = snd . foldr foldingF (-100, Move (Coord 1 1) (Coord 1 1))
               foldingF x y = if fst y > fst x then y else x
-       
-abMaxNode :: Int -> Int -> Int -> Maybe Game -> Int
-abMaxNode 0 a b (Just g) = evalNaive'' g
-abMaxNode d a b (Just g@(Game _ _ trn))
+
+abNode :: Int -> Int -> Int -> Game -> Bool -> Int
+abNode 0 _ _ g _ = evalNaive g
+abNode d a b g@(Game _ _ trn) isMax
         | checkMate g trn = -1000
         | checkMate g (notC trn) = 1000
-        | otherwise = abMaxHelper d (-100) a b g $ nextMoves g
+        | isMax = abMaxHelper d (-100) a b g $ nextMoves g
+        | otherwise = abMinHelper d 100 a b g $ nextMoves g
+
         
 abMaxHelper :: Int -> Int -> Int -> Int -> Game -> [Move] -> Int
 abMaxHelper d v a b g [] = v
 abMaxHelper d v a b g (x:xs)
         | b <= newA = newV
         | otherwise = abMaxHelper d newV newA b g xs
-                where newV = max v (abMinNode (d - 1) a b (move g x))
+                where newV = max v (abNode (d - 1) a b (move g x) False)
                       newA = max a newV
                      
-abMinNode :: Int -> Int -> Int -> Maybe Game -> Int
-abMinNode 0 a b (Just g) = evalNaive'' g
-abMinNode d a b (Just g@(Game _ _ trn))
-        | checkMate g trn = -1000
-        | checkMate g (notC trn) = 1000 
-        | otherwise = abMinHelper d 100 a b g $ nextMoves g 
-
 abMinHelper :: Int -> Int -> Int -> Int -> Game -> [Move] -> Int
 abMinHelper d v a b g [] = v
 abMinHelper d v a b g (x:xs)
         | newB <= a = newV
         | otherwise = abMinHelper d newV a newB g xs
-                where newV = min v (abMaxNode (d - 1) a b (move g x))
+                where newV = min v (abNode (d - 1) a b (move g x) True)
                       newB = min b newV
                       
-
 nextMoves :: Game -> [Move]
 nextMoves g = filterLegal $ concatMap (possibleMoves g) allSpaces
         where filterLegal = foldr (\cur lst -> case move g cur of
@@ -77,30 +59,13 @@ children g = filterJusts . map (move g) $ concatMap (possibleMoves g) allSpaces
         where filterJusts = foldr (\cur lst -> case cur of
                  Just gm -> gm:lst
                  _ -> lst) []
-
-evalNaive :: Board -> Color -> Int
-evalNaive b clr = loop 0 [1..8] (\total x -> 
-        loop total [1..8] (\total2 y ->
-                case color <$> getP b (Coord x y) of
-                        Just clr' -> if clr == clr' then total2 + 1 else total2 - 1
-                        Nothing -> total2))
-
-evalNaive' :: Board -> Color -> Int
-evalNaive' b clr = loop 0 [1..8] (\total x -> 
-        loop total [1..8] (\total2 y ->
-                case color <$> getP b (Coord x y) of
-                        Just clr' -> if clr == clr' then value (getP b (Coord x y)) + total2 else (value $ getP b (Coord x y)) * (-1) + total2
-                        Nothing -> total2))
                         
-evalNaive'' :: Game -> Int
-evalNaive'' g@(Game b _ clr)
+evalNaive :: Game -> Int
+evalNaive g@(Game b _ clr)
         | checkMate g clr = -1000
         | checkMate g (notC clr) = 1000
-        | otherwise = loop 0 [1..8] (\total x -> 
-                loop total [1..8] (\total2 y ->
-                        case color <$> getP b (Coord x y) of
-                                Just clr' -> if clr == clr' then value (getP b (Coord x y)) + total2 else (value $ getP b (Coord x y)) * (-1) + total2
-                                Nothing -> total2))
+        | otherwise = let values p = value p * (if (color <$> p) == Just clr then 1 else -1) in
+                sum $ map values [getP b (Coord x y) | x <- [1..8], y <- [1..8]]
 
 value :: Maybe Piece -> Int
 value (Just (Pawn _)) = 1
